a7a412004bf5ec9ace40c1895f110ba6852d1280
olayeancarh <akejuolayinka016@gmail.com>
2019-04-12 15:42:53 +0100

    Secured api routes

---
+++ src/api/utilz/middleware.js
@@ -0,0 +1,34 @@
+/* eslint-disable consistent-return */
+import jwt from 'jsonwebtoken';
+import config from './config';
+
+function checkToken(req, res, next) {
+  // Get auth header value
+  const bearerHeader = req.headers.authorization;
+  // Check if bearer is undefined
+  if (typeof bearerHeader !== 'undefined') {
+    // Split at the space
+    const bearer = bearerHeader.split(' ');
+    // Get token from array
+    const bearerToken = bearer[1];
+    jwt.verify(bearerToken, config.secret, (err, decoded) => {
+      if (err) {
+        return res.json({
+          success: false,
+          message: `Token is not valid ${err}`,
+        });
+      }
+      req.decoded = decoded;
+      next();
+    });
+  } else {
+    return res.json({
+      success: false,
+      message: 'Auth token is not supplied',
+    });
+  }
+}
+
+export default {
+  checkToken,
+};
