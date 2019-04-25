955fdb34c53eba9eac40e640076f0f650300901b
olayeancarh <akejuolayinka016@gmail.com>
2019-04-25 13:27:33 +0100

    feature(user signup): fix travis issues

--- src/api/db/db.js
+++ src/api/db/db.js
@@ -9,17 +9,17 @@ const userEnv = {
     ssl: false,
   },
   production: {
-    connectionString: 'postgresql://postgres:admins@127.0.0.1:5432/banka',
+    connectionString: 'postgres://wrzrvclkxnmkrs:e2fdde48ebab7fcfeed46a60bc907c6b254bdd6d4da7b43ff25770572837389c@ec2-54-243-197-120.compute-1.amazonaws.com:5432/d1tn3c0l0she6u',
     ssl: true,
   },
   test: {
-    connectionString: 'postgresql://postgres:admins@127.0.0.1:5432/banka',
+    connectionString: 'postgres://gucbgfcy:cjy3nuYOfPBoEhS2omIS-QTrlc3ngBqx@isilo.db.elephantsql.com:5432/gucbgfcy',
     ssl: false,
   },
 };
 
 const pool = new Pool({
-  connectionString: userEnv[process.env.NODE_ENV].connectionString,
+  connectionString: userEnv[env].connectionString,
   ssl: userEnv[env].ssl,
 });
 
